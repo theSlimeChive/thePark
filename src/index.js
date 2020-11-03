0c7f178fbdea5a0e2b44baf9c129c224e5332c9b
knottyChive <jalengilyard99@gmail.com>
2020-11-03 14:40:21 +0000

    Icons and Pictures Added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/navbar.component';
+import portalIcon from './public/images/pixlr-bg-result.png';
+import characterIcon from './public/images/characters-icon.png';
+import doggo from './public/images/dog-fial.png';
 
 export default class Main extends React.Component {
     render() {
@@ -8,6 +11,9 @@ export default class Main extends React.Component {
             <div>
                 <Navbar />
                 <p>This is the App area</p>
+                <img src={portalIcon} />
+                <img src={characterIcon} />
+                <img src={doggo} />
             </div>
         )
     }
