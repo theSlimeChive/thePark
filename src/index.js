ebba4327bab8ca238a6d608ab6c2038825e7716a
knottyChive <jalengilyard99@gmail.com>
2020-11-10 05:30:35 +0000

    Connected to API and data stored

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/navbar.component';
-import portalIcon from './public/images/pixlr-bg-result.png';
-import characterIcon from './public/images/characters-icon.png';
-import doggo from './public/images/dog-fial.png';
+
+const axios = require('axios');
+
 
 export default class Main extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            characters: []
+        }
+    }
+    componentDidMount() {
+        axios.get('https://rickandmortyapi.com/api/character/')
+        .then(result => {
+            this.setState({
+                characters: result.data.results
+            })
+            console.log(this.state.characters)
+        })
+        .catch(err => console.error(err))
+    }
+
     render() {
         return (
             <div>
